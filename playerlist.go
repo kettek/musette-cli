b8cf62ff673b046b2abff71c70283760e3604c97
kts of kettek (yan) <kettek1@kettek.net>
2019-12-02 20:48:03 -0800

    Add PlayerList type

---
+++ playerlist.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
+
+type PlayerList struct {
+	table  *tview.Table
+	tracks []Track
+}
+
+func (p *PlayerList) Setup() {
+	p.table = tview.NewTable().SetSelectable(true, false)
+
+	// temp
+	for i := 0; i < 30; i++ {
+		p.tracks = append(p.tracks, Track{
+			trackNumber: int8(i),
+			trackName:   "Name",
+			trackArtist: "Artist",
+			trackAlbum:  "Album",
+		})
+	}
+
+	p.syncList()
+}
+
+func (p *PlayerList) syncList() {
+	p.table.Clear()
+
+	// Add tracks
+	for i, t := range p.tracks {
+		for c := 0; c < 5; c++ {
+			color := tcell.ColorWhite
+			cell := tview.NewTableCell("").
+				SetTextColor(color).
+				SetAlign(tview.AlignLeft)
+			switch c {
+			case 0:
+				cell.SetText(fmt.Sprintf("%d", t.trackNumber))
+			case 1:
+				cell.SetText(t.trackName)
+				cell.SetExpansion(100)
+			case 2:
+				cell.SetText(t.trackArtist)
+			case 3:
+				cell.SetText(t.trackAlbum)
+			case 4:
+				if t.selected {
+					cell.SetText("x")
+				} else {
+					cell.SetText(" ")
+				}
+			}
+			p.table.SetCell(i, c, cell)
+		}
+	}
+}
